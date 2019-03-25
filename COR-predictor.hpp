8d6dfcbfc1da035022d990d3d986c05d63ca36b5
SchroedingersFerret <36749972+SchroedingersFerret@users.noreply.github.com>
2019-03-25 11:44:08 -0500

    Update COR-predictor.hpp

--- COR-predictor.hpp
+++ COR-predictor.hpp
@@ -33,40 +33,10 @@
 #include <stdlib.h>
 #include <thread>
 
-
-//initial population size
-static int n_initial;
-//size of gene pool for selection
-static int n_gpool;
-//number of chromosomes to be selected for reproduction
-static int n_repro;
-//number of independent variables
-static const int nx = 7;
-//percentage selected for mutation
-static double pm;
-//number of elites
-static int n_elite;
-//remainder of population
-static int n_normal;
-//number of datapoints
-static int n_data;
-//least squares error tolerance
-static double error;
-//independent variables array size n_data*nx
-static std::vector<std::vector<double> > independent;
-//dependent variable array size n_data
-static std::vector<double> dependent;
-//determines whether initial population contains entirely random parameters
-static bool random_parameters = false;
-//quits the program
-static bool quit_cor = false;
-
-//parameter array
-static std::vector<std::vector<double> > parameters_global(4,std::vector<double> (5));
-
 class COR_predictor
-{
+{	
 	private:
+		
 		static std::vector<double> read_csv1d(const char * filename);
 		static std::vector<std::vector<double> > read_csv2d(const char * filename);
 		static void write_csv1d(std::vector<double> a, const char * filename);
@@ -88,7 +58,37 @@ class COR_predictor
 		static void Predict();
 		static void Show_main_menu();
 		struct Mode;
+		
 	public:
+		//initial population size
+		static int n_initial;
+		//size of gene pool for selection
+		static int n_gpool;
+		//number of chromosomes to be selected for reproduction
+		static int n_repro;
+		//number of independent variables
+		static const int nx = 7;
+		//percentage selected for mutation
+		static double pm;
+		//number of elites
+		static int n_elite;
+		//remainder of population
+		static int n_normal;
+		//number of datapoints
+		static int n_data;
+		//least squares error tolerance
+		static double error;
+		//independent variables array size n_data*nx
+		static std::vector<std::vector<double> > independent;
+		//dependent variable array size n_data
+		static std::vector<double> dependent;
+		//determines whether initial population contains entirely random parameters
+		static bool random_parameters;
+		//quits the program if true
+		static bool quit_cor;
+		//parameter array
+		static std::vector<std::vector<double> > parameters_global;
+		
 		static void Get_settings();
 		static void Get_independent();
 		static void Get_dependent();
@@ -96,5 +96,19 @@ class COR_predictor
 		static void Main_menu();
 };
 
+int COR_predictor::n_initial;
+int COR_predictor::n_gpool;
+int COR_predictor::n_repro;
+double COR_predictor::pm;
+int COR_predictor::n_elite;
+int COR_predictor::n_normal;
+int COR_predictor::n_data;
+double COR_predictor::error;
+std::vector<std::vector<double> > COR_predictor::independent;
+std::vector<double> COR_predictor::dependent;
+bool COR_predictor::random_parameters = false;
+bool COR_predictor::quit_cor = false;
+std::vector<std::vector<double> > COR_predictor::parameters_global(4,std::vector<double> (5);
+								   
 #endif /* COR_PREDICTOR_HPP_ */
 
