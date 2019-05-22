1c88f7676b101007a9aed4528cc9a3ab08988403
SchroedingersFerret <36749972+SchroedingersFerret@users.noreply.github.com>
2019-05-22 11:33:54 -0500

    Update COR-main.cpp

--- COR-main.cpp
+++ COR-main.cpp
@@ -33,7 +33,7 @@ int main()
 	COR_predictor::Get_independent();
 	COR_predictor::Get_dependent();
 	COR_predictor::Get_parameters();
-	std::cout << "Welcome to COR Predictor 0.5-beta\n";
+	std::cout << "Welcome to COR Predictor 0.5.1-beta\n";
 	std::cout << "Copyright 2019, J. Ball (SchroedingersFerret)\n\n";
 	while (!COR_predictor::quit_cor)
 		COR_predictor::Main_menu();
